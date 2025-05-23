12e0b5811624d78ef90681b6600bb22825cda16c
Ruslan <120032162+INRGI@users.noreply.github.com>
2025-05-23 10:49:20 +0200

    [update] delete test service

--- apps/backend/src/app/modules/finances/broadcast/controllers/broadcast.controller.ts
+++ apps/backend/src/app/modules/finances/broadcast/controllers/broadcast.controller.ts
@@ -1,17 +1,11 @@
 import { Controller, Get, Param } from "@nestjs/common";
 import { GetAllDomainsService } from "../services/get-all-domains/get-all-domains.service";
 import { GetAllDomainsResponse } from "@epc-services/interface-adapters";
-import {
-  InjectBigQueryApiService,
-  BigQueryApiServicePort,
-} from "@epc-services/bigquery-api";
 
 @Controller("finances/broadcast")
 export class BroadcastController {
   constructor(
     private readonly getAllDomainsService: GetAllDomainsService,
-    @InjectBigQueryApiService()
-    private readonly statisticApiService: BigQueryApiServicePort
   ) {}
 
   @Get(":team")
@@ -21,15 +15,6 @@ export class BroadcastController {
     const result = await this.getAllDomainsService.getDomains({
       broadcastTeam: team,
     });
-    const today = new Date();
-    const weekAgo = new Date();
-    weekAgo.setDate(today.getDate() - 7);
-
-    const from = weekAgo.toISOString().split("T")[0]; // формат YYYY-MM-DD
-    const to = today.toISOString().split("T")[0];
-
-    const test = await this.statisticApiService.getStats({ from, to });
-    // console.log(test);
     return await result;
   }
 }
