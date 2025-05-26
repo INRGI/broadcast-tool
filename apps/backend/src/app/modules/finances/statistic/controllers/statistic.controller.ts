19abc39e7b95795eb076489e23a80fc35955a89d
Ruslan <120032162+INRGI@users.noreply.github.com>
2025-05-26 09:08:18 +0200

    [delete] stats module

--- apps/backend/src/app/modules/finances/statistic/controllers/statistic.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller} from "@nestjs/common";
-
-@Controller("finances/statistic")
-export class StatisticController {}
--- apps/backend/src/app/modules/finances/statistic/statistic-providers.ts
+++
@@ -1,8 +0,0 @@
-import { Provider } from "@nestjs/common";
-import { StatisticController } from "./controllers/statistic.controller";
-
-export const messageControllers = [StatisticController];
-
-export const applicationProviders: Provider[] = [];
-
-export const serviceProviders: Provider[] = [];
