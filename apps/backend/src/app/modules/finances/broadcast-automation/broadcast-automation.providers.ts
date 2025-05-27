b086356b74b4ff7fbfbff0d43718d3146f33f0e7
Ruslan <120032162+INRGI@users.noreply.github.com>
2025-05-27 11:42:31 +0200

    [delete] module

--- apps/backend/src/app/modules/finances/broadcast-automation/broadcast-automation.providers.ts
+++
@@ -1,7 +0,0 @@
-import { Provider } from "@nestjs/common";
-
-export const messageControllers = [];
-
-export const applicationProviders: Provider[] = [];
-
-export const serviceProviders: Provider[] = [];
