ec2915c0307ce76b3b0679f2a1b7c2f0e622d6b3
Ruslan <120032162+INRGI@users.noreply.github.com>
2025-05-27 11:42:57 +0200

    [delete] module

--- apps/backend/src/app/modules/finances/finances.module.ts
+++ apps/backend/src/app/modules/finances/finances.module.ts
@@ -2,10 +2,9 @@ import { Module } from "@nestjs/common";
 import { MondayModule } from "./monday/monday.module";
 import { PriorityModule } from "./priority/priority.module";
 import { BroadcastModule } from "./broadcast/broadcast.module";
-import { BroadcastAutomationModule } from "./broadcast-automation/broadcast-automation.module";
 
 @Module({
-  imports: [MondayModule, PriorityModule, BroadcastModule, BroadcastAutomationModule],
+  imports: [MondayModule, PriorityModule, BroadcastModule],
   controllers: [],
   providers: [],
   exports: [],
